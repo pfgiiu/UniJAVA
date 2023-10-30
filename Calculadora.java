4027a35ae31f2ed475b5fd6ec9699d473b07c7d8
Giulia <giuliapforte@gmail.com>
2023-10-30 14:40:44 -0300

    inicio

---
+++ Calculadora.java
@@ -0,0 +1,9 @@
+public class Calculadora {
+    public int somar(int a, int b) {
+        return a + b;
+    }
+
+    public int subtrair(int a, int b) {
+        return a - b;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Calculadora calculadora = new Calculadora();
+
+
+        int resultadoSoma = calculadora.somar(5, 3);
+        int resultadoSubtracao = calculadora.subtrair(10, 4);
+
+  
+        System.out.println("Resultado da Soma: " + resultadoSoma);
+        System.out.println("Resultado da Subtração: " + resultadoSubtracao);        
+    }
+}
